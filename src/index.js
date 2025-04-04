4462312b02048aebc58fe701aa5ba9a00fc6aeab
varundachuu <varundachuu2000@gmail.com>
2025-04-05 01:25:16 +0530

    css updates, link update comit

--- src/index.js
+++ src/index.js
@@ -5,11 +5,23 @@ import "./index.css"; // Import your custom CSS
 function App() {
   return (
     <div className="main-container">
+      {/* Top Center Link */}
+      <div className="top-link">
+        <a href="https://klg.co.in" target="_blank" rel="noopener noreferrer">
+          klg.co.in
+        </a>
+      </div>
+
       <div className="overlay">
         <div className="title-box">
-          <h1>Maintenance Mode Is On</h1>
-          <p>Site will be available soon. Thankyou for your patience</p>
+          <h1>Maintenance mode is on</h1>
+          <p>Site will be available soon. Thankyou for your patience!</p>
         </div>
+      </div>
+
+      {/* Bottom Center Link */}
+      <div className="bottom-link">
+        <a href="mailto:support@klg.co.in">klg.co.in</a>
       </div>
     </div>
   );
